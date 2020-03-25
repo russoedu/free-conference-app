b174c26ea8a552d0afe810b3db82ac01f50b231e
Eduardo Russo <russoedu@gmail.com>
2020-03-25 09:45:08 +0000

    Improved positioning and timer display

--- injection-general.js
+++ injection-general.js
@@ -1,8 +1,8 @@
 const config = require('./config')
 
-const tabeleInnerHTML = `<webview id="table" src="${config.spreadsheetUrl}" autosize="on" style="width:calc(30% + 58px); height:100vh;" preload="./injection-table.js"></webview>`
-const conferenceInnerHTML = `<webview id="conference" src="https://hello.freeconference.com/conf/call/${config.conferenceId}" preload="./injection-conference.js" autosize="on" style="width:70%; height:80vh"></webview>`
-const timerInnerHTML = '<webview id="timer" src="https://www.google.com/search?q=timer" preload="./injection-timer.js" autosize="on" style="width:100%; height:20vh"></webview>'
+const tabeleInnerHTML = `<webview id="table" src="${config.spreadsheetUrl}" autosize="on" style="width:calc(560px + 55px); height:100vh;" preload="./injection-table.js"></webview>`
+const conferenceInnerHTML = `<webview id="conference" src="https://hello.freeconference.com/conf/call/${config.conferenceId}" preload="./injection-conference.js" autosize="on" style="width:100%; height:calc(100vh - 154px)"></webview>`
+const timerInnerHTML = '<webview id="timer" src="https://www.google.com/search?q=timer" preload="./injection-timer.js" autosize="on" style="height:154px"></webview>'
 
 setTimeout(function waitForTable () {
   if (document.getElementById('table-injection')) {
--- injection-timer.js
+++ injection-timer.js
@@ -10,15 +10,15 @@
 
 setTimeout(function removeFromPage () {
   if (document.querySelector('#sfcnt')) {
-    const style = document.createElement('STYLE');
-    const head = document.getElementsByTagName('HEAD')[0];
+    const style = document.createElement('STYLE')
+    const head = document.getElementsByTagName('HEAD')[0]
     const styleText = `html {
       height: 0!important;
       overflow: hidden!important;
     }
     #cst, #sfcnt, #top_nav, #searchform, .srg, #appbar, #rso > div:nth-child(3) > div,
       #gsr > div:nth-child(2), #search > div > h1, #extrares, #act-tab-header, #act-fbk-row, #rso > div:nth-child(2),
-      #center_col > div:nth-child(5), #footcnt, #navcnt, #rso > div:nth-child(3), #rso > div.g > div, #xjs > div > table {
+      #center_col > div:nth-child(5), #footcnt, #navcnt, #rso > div:nth-child(3), #rso > div.g > div, #xjs > div > table, #rso > div.LnbJhc, #act-timer-section > div > div.act-tim-btn-cnt.act-btn-row > span.act-tim-btn-right > a.act-btn.act-tim-not-fullscreen {
       display:none!important;
     }
     #cnt, #res {
@@ -38,10 +38,13 @@ setTimeout(function removeFromPage () {
     }
     #rcnt {
       height: 0!important;
+    }
+    #act-timer-section > div {
+        width: 300px;
     }`;
-    const textnode = document.createTextNode(styleText);
-    style.appendChild(textnode);
-    head.appendChild(style);
+    const textnode = document.createTextNode(styleText)
+    style.appendChild(textnode)
+    head.appendChild(style)
   } else {
     setTimeout(removeFromPage, 500)
   }
